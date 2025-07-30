33f79264a97c8696a9fd72e11156f5835ec0a9be
Chris Buckland <cpbuckland88@gmail.com>
2025-07-30 14:28:37 +0100

    feat: implement ArbGasInfo.getMinimumGasPrice
      - Add ArbGasInfo contract implementing IArbGasInfo interface
      - Add minBaseFeeWei getter to L2PricingState library
      - Add differential test for getMinimumGasPrice function

---
+++ contracts/ArbGasInfo.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ArbGasInfo as IArbGasInfo} from "../submodules/nitro-precompile-interfaces/ArbGasInfo.sol";
+import {ArbosState} from "./libraries/ArbosState.sol";
+import {L2PricingState, L2PricingStorage} from "./libraries/L2PricingState.sol";
+
+contract ArbGasInfo is IArbGasInfo {
+    using L2PricingState for L2PricingStorage;
+
+    function getPricesInWeiWithAggregator(
+        address aggregator
+    ) external view override returns (uint256, uint256, uint256, uint256, uint256, uint256) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getPricesInWei()
+        external
+        view
+        override
+        returns (uint256, uint256, uint256, uint256, uint256, uint256)
+    {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getPricesInArbGasWithAggregator(
+        address aggregator
+    ) external view override returns (uint256, uint256, uint256) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getPricesInArbGas() external view override returns (uint256, uint256, uint256) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getGasAccountingParams() external view override returns (uint256, uint256, uint256) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getMinimumGasPrice() external view override returns (uint256) {
+        return ArbosState.l2PricingState().minBaseFeeWei();
+    }
+
+    function getL1BaseFeeEstimate() external view override returns (uint256) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getL1BaseFeeEstimateInertia() external view override returns (uint64) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getL1RewardRate() external view override returns (uint64) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getL1RewardRecipient() external view override returns (address) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getL1GasPriceEstimate() external view override returns (uint256) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getCurrentTxL1GasFees() external view override returns (uint256) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getGasBacklog() external view override returns (uint64) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getPricingInertia() external view override returns (uint64) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getGasBacklogTolerance() external view override returns (uint64) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getL1PricingSurplus() external view override returns (int256) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getPerBatchGasCharge() external view override returns (int64) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getAmortizedCostCapBips() external view override returns (uint64) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getL1FeesAvailable() external view override returns (uint256) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getL1PricingEquilibrationUnits() external view override returns (uint256) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getLastL1PricingUpdateTime() external view override returns (uint64) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getL1PricingFundsDueForRewards() external view override returns (uint256) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getL1PricingUnitsSinceUpdate() external view override returns (uint64) {
+        revert("NOT_IMPLEMENTED");
+    }
+
+    function getLastL1PricingSurplus() external view override returns (int256) {
+        revert("NOT_IMPLEMENTED");
+    }
+}
