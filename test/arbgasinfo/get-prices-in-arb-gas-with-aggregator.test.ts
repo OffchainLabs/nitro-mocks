b3ae60635ab8d8a1f21299cc469f036dfce892f7
Chris Buckland <cpbuckland88@gmail.com>
2025-07-31 12:57:17 +0100

    test: add test for ArbGasInfo.getPricesInArbGasWithAggregator
      Add differential test for getPricesInArbGasWithAggregator function to ensure the existing implementation matches native behavior. The function was already implemented but lacked test coverage.

---
+++ test/arbgasinfo/get-prices-in-arb-gas-with-aggregator.test.ts
@@ -0,0 +1,25 @@
+import { deployAndSetCode, PRECOMPILE_ADDRESSES } from "../utils/utils";
+import { expectEquivalentCallFromMultipleAddresses, storageAccessComparerExcludingVersion } from "../utils/expect-equivalent";
+import { ArbGasInfo__factory } from "../../typechain-types";
+import { ethers } from "hardhat";
+
+describe("ArbGasInfo.getPricesInArbGasWithAggregator", function () {
+  beforeEach(async function() {  
+    await deployAndSetCode("ArbosStorage", "0xA4b05FffffFffFFFFfFFfffFfffFFfffFfFfFFFf");
+    await deployAndSetCode("contracts/ArbGasInfo.sol:ArbGasInfo", PRECOMPILE_ADDRESSES.ArbGasInfo);
+  });
+
+  it("should match native implementation", async function () {
+    const aggregator = ethers.Wallet.createRandom().address;
+    
+    await expectEquivalentCallFromMultipleAddresses(
+      ArbGasInfo__factory,
+      PRECOMPILE_ADDRESSES.ArbGasInfo,
+      "getPricesInArbGasWithAggregator",
+      [aggregator],
+      {
+        storageAccess: storageAccessComparerExcludingVersion
+      }
+    );
+  });
+});
